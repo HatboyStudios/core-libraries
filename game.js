526357528500164567d86e770d86a143c54a30b5
UltraHispeed-Elite <62296749+UltraHispeed-Elite@users.noreply.github.com>
2024-08-02 07:47:48 -0700

    objects

--- game.js
+++ game.js
@@ -1,13 +1,19 @@
+var player;
+
 function init() {
     if(debug === true) {
         console.log("init is active");
     }
     generateCanvas(400,400);
     background("green");
+
+    player = new Object(200,200,50,50);
 }
 
 function loop() {
     if(debug === true) {
         console.log("loop function active");
     }
+
+    player.drawObject();
 }
