10ecba51940f5432636f75fded8ab02ef0acff50
UltraHispeed-Elite <62296749+UltraHispeed-Elite@users.noreply.github.com>
2024-08-06 05:40:37 -0700

    sprite movement with keyboard

--- game.js
+++ game.js
@@ -1,21 +1,27 @@
 var player;
+var player2;
 
 function init() {
     if(debug === true) {
         console.log("init is active");
     }
     generateCanvas(400,400);
-    background("green");
 
-    player = new Object(200,200,50,50);
+    player = new Sprite(200,200,50,50);
+    player.color = "purple";
+
+    player2 = new Sprite(100,100,50,50);
 }
 
 function loop() {
+    background("green");
     if(debug === true) {
         console.log("loop function active");
     }
 
+    player.movement();
     player.drawObject();
+    player2.drawObject();
 
     if(playKey("w")) {
         console.log("w is pressed");
